9d224e53c6e141c4778b75e62634f8f8db15d576
Hamed Jimoh <58299107+hmdNetizen@users.noreply.github.com>
2021-02-17 06:05:47 -0800

    Created a route for deleting a post based on the user that posted it

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,4 +39,57 @@ router.post(
   }
 );
 
+// @TYPE     GET /api/posts
+// @Desc     Get all posts
+// @access   Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error!");
+  }
+});
+
+// @TYPE     GET /api/posts/:id
+// @Desc     Get post by id
+// @access   Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId")
+      return res.status(404).json({ msg: "Post not found" });
+    res.status(500).send("Server Error!");
+  }
+});
+
+// @TYPE     DELETE /api/posts/:id
+// @Desc     Delete a post
+// @access   Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: "Post not found" });
+
+    if (post.user.toString() !== req.user.id)
+      return res.status(401).json({ msg: "Unauthorized Access" });
+
+    await post.remove();
+
+    res.json("Post Deleted");
+  } catch (error) {}
+});
+
 module.exports = router;
